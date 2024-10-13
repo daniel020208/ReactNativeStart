4d651bc4abf19a9b9306f868870f96b017d27fb2
Daniel <daniel.golod2008@gmail.com>
2024-10-13 19:14:36 +0300

    Try

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function Header() {
       <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {description} React concepts you will need for almost any app you are
+        {description} something React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
