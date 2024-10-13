4a8aae2b2ba9e62f2d20a7ffec902dbecf9f9128
Daniel <daniel.golod2008@gmail.com>
2024-10-13 19:15:17 +0300

    Commit something

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function Header() {
       <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {description} something React concepts you will need for almost any app you are
+        {description}  test  1 React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
