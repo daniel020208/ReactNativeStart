6612783966be1dd9c3d78bf6391b56a8e3c84e17
Daniel <daniel.golod2008@gmail.com>
2024-10-16 14:04:16 +0300

    10.16

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,46 @@
-import reactImg from './assets/react-core-concepts.png';
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
 
-const reactDescription = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description=reactDescription[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description}  test  1 React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
+import { CORE_CONCEPTS } from './data';
+import { Component } from 'react';
 
 
 function App() {
+  let tabContent ='Please clic a button';
+
+
+  function handleSelect(buttonName){
+    tabContent=buttonName;
+  }
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept{...CORE_CONCEPTS[0]}/>
+            <CoreConcept{...CORE_CONCEPTS[1]}/>
+            <CoreConcept{...CORE_CONCEPTS[2]}/>
+            <CoreConcept
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image}
+            />
+
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          
+        </section>
       </main>
     </div>
   );
