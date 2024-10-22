00dc9a880c2f2c9c88851a2ea93620db1fec4bb2
Daniel <daniel.golod2008@gmail.com>
2024-10-22 14:01:42 +0300

    10.22

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
+import { useState } from 'react';
+
+import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data';
 
-import { CORE_CONCEPTS } from './data';
-import { Component } from 'react';
 
 
 function App() {
-  let tabContent ='Please clic a button';
+  const [selectedTopic,setSelectedTopic] = useState('components');
 
 
   function handleSelect(buttonName){
-    tabContent=buttonName;
+    setSelectedTopic(buttonName);
   }
   return (
     <div>
@@ -39,7 +41,15 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          
+          <div id='tab-content'>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
